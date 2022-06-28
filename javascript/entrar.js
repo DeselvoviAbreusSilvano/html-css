2251afe859b73d03e314d856d86f140081d0ca28
Desevowebabreus@ <108025623+DeselvoviAbreusSilvano@users.noreply.github.com>
2022-06-28 18:14:22 +0100

    Atualizei o meu javascript

--- javascript/entrar.js
+++ javascript/entrar.js
@@ -39,6 +39,9 @@ function entrar(){
     
     if (Usuario.value == uservalid.user && Senha.value == uservalid.senha ){
          window.location.href ='https://deselvoviabreussilvano.github.io/html-css/nossa%20loja.html'
+         
+         let token = Math.rendom().toString(16).substr(2) + Math.rendom().toString(16).substr(2)
+         localStorage.setItem('token',token)
     } else {
         tabelausuario.setAttribute('style', 'color: red')
         Usuario.setAttribute('style', 'border-color: red')
