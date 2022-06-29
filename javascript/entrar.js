33e62db3f9439cd5fa94b26e229150ed729b8633
Desevowebabreus@ <108025623+DeselvoviAbreusSilvano@users.noreply.github.com>
2022-06-29 11:19:20 +0100

    Atualizei o meu entrar.js

--- javascript/entrar.js
+++ javascript/entrar.js
@@ -55,7 +55,7 @@ function entrar(){
     let userLogado = JSON.parse(localStorage.getItem('userLogado'))
 
     let logado = document.querySelector('logado')
-    logado.innerHTML= `Olá ${userLogado.nome}`
+    logado.innerHTML= `Olá``${userLogado.nome}` 
 
     if(localStorage.getItem('token') == null){
         alert('Você precisa estar logado para acessar esta página')
