0b1740418ae312bb5e5a1ac1dc53b93f165f520b
Desevowebabreus@ <108025623+DeselvoviAbreusSilvano@users.noreply.github.com>
2022-06-29 02:08:51 +0100

    Atualizei o meu entrar.js

--- javascript/entrar.js
+++ javascript/entrar.js
@@ -30,8 +30,8 @@ function entrar(){
 
     if (Usuario.value == uservalid.user && Senha.value == uservalid.senha) {
 
-        window.Location.href="https://deselvoviabreussilvano.github.io/html-css/nossa%20loja.html"
-        
+        Window.Location.href="https://deselvoviabreussilvano.github.io/html-css/nossa%20loja.html"
+
         let token = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2)
         localStorage.setItem('token',token)
         
@@ -48,4 +48,11 @@ function entrar(){
     }
 
     }
- 
+    if(localStorage.getItem('token') == null){
+        alert('Você precisa estar logado para acessar esta página')
+        Window.Location.href ="https://deselvoviabreussilvano.github.io/html-css/log.html"
+    }
+    function sair(){
+        localStorage.removeItem('token')
+    Window.Location.href ="https://deselvoviabreussilvano.github.io/html-css/log.html"
+    }
