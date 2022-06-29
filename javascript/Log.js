a50db36acf33cf90cd5e4bbff02d83c4d938a673
Desevowebabreus@ <108025623+DeselvoviAbreusSilvano@users.noreply.github.com>
2022-06-29 13:43:07 +0100

    crie o meu Log.js

---
+++ javascript/Log.js
@@ -0,0 +1,15 @@
+
+let userlogado = JSON.parse(localStorage.getItem('userlogado'))
+
+    let logado = document.querySelector('#logado')
+   logado.innerHTML = `Olá ${userlogado.nome}` 
+
+    if(localStorage.getItem('token') == null){
+        alert('Você precisa estar logado para acessar esta página')
+        window.location.href="https://deselvoviabreussilvano.github.io/html-css/log.html"
+    }
+    function sair(){
+        localStorage.removeItem('token')
+        localStorage.removeItem('userLogado')
+    window.Location.href ="https://deselvoviabreussilvano.github.io/html-css/log.html"
+    }
