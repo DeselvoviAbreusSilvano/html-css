b5407c39ed76108090bb5db3abd3c35544be8ca1
Desevowebabreus@ <108025623+DeselvoviAbreusSilvano@users.noreply.github.com>
2022-07-23 18:51:52 +0100

    Atualizei o meu site

--- javascript/animação.js
+++ javascript/animação.js
@@ -1,24 +1,18 @@
- var intervalo = 3000;
+//  Animação todos os meu tens da telas 
+const item = document.querySelectorAll("[data-anime]");
+const animeScroll = () =>{
+    const windowTop = window.pageXOffset + window.innerHeight * 0.85;
 
-function  slaide1() {
-    document. getElementById("banner") .src="../imagens/benner1.jpg";
-    setTimeout("slaide2 ()",intervalo); 
-}
+    item.forEach((element) =>{
+        if (windowTop < element.offsetTop ) {
+            element.classList.add("animate");
+        } else{
+            element.classList.remove("animate");
+        }
+    });
+};
+animeScroll();
 
-function  slaide2(){
-    document. getElementById("banner") .src="../imagens/benner2.png";
-    setTimeout("slaide3 ()",intervalo);
-}
-
-function  slaide3(){
-    document. getElementById("banner") .src="../imagens/benner3.png";
-        setTimeout ("slaide4 ()",intervalo);
-}
-function slaide4(){
-    document. getElementById("banner") .src="../imagens/benner4.png" ;
-    setTimeout( "slaide5 ()",intervalo);
-}
-function slaide5(){
-    document. getElementById("banner") .src="../imagens/benner5.jpg";
-    setTimeout("slaide1 ()",intervalo);
-}
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
